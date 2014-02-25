2ae2ec9588f7aa1ee8aac6ba99b3a5f739ad36f9
tiffanie <yt.chia.2009@smu.edu.sg>
2014-02-25 15:07:39 +0000

    fizzbuzz prog

---
+++ fizzbuzz.rb
@@ -0,0 +1,40 @@
+def play(num)
+
+ 	
+	return 'FizzBuzz' if divisable_by_fifteen?(num)
+	return 'Fizz' if divisable_by_three?(num)
+ 	return 'Buzz' if divisable_by_five?(num)
+ 	num	
+# return puts num if divisable_by_three?(num) && !divisable_by_five?(num) && !divisable_by_fifteen?(num) 
+end
+
+def divisable_by_fifteen?(num)
+	num % 15 == 0
+
+end
+
+def divisable_by_three?(num)
+	num % 3 == 0 
+end
+
+def divisable_by_five?(num)
+	num % 5 == 0
+end
+ 
+# method 1 looping
+num = 1 
+loop do
+	puts play(num)
+	num += 1
+	break if num >= 100
+end
+
+# method 2 looping
+# num = 1
+# while num < 100
+# 	puts play(num)
+# 	num += 1
+# end
+
+#method 3 looping
+# (1..100).each{|num|puts play(num)}
